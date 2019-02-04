61eb095dc338b0809d38449e04e316dd3c692c4b
Jack Snowdon <jacksnowdondrums@gmail.com>
2019-02-04 14:15:44 +0000

    Added spell class

--- classes/game.py
+++ classes/game.py
@@ -1,4 +1,6 @@
 import random
+from .magic import Spell
+import pprint
 
 class bcolors:
     HEADER = '\033[95m'
@@ -32,11 +34,6 @@ class Person:
         
     def generate_dmg(self):
         return random.randrange(self.atkl, self.atkh)
-    
-    def generate_spell_dmg(self, i):
-        ngl = self.magic[i]["dmg"] - 5
-        ngh = self.magic[i]["dmg"] + 5
-        return random.randrange(ngl, ngh)
         
     def take_damage(self, dmg):
         self.hp -= dmg
@@ -64,12 +61,6 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
         
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-        
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
-        
     def choose_action(self):
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
@@ -81,7 +72,7 @@ class Person:
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) + ")")
+            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
             i += 1
             
             
