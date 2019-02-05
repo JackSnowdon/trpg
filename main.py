ad58d043653fd847a27a1e5e7710b0c5d88ae23d
Jack Snowdon <jacksnowdondrums@gmail.com>
2019-02-05 12:52:01 +0000

    added working HP/MP party bars

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from classes.inventory import Item
 
 
 # Create Black Magic
-fire = Spell("Fire", 10, 100, "black")
-thunder = Spell("Thunder", 12, 120, "black")
-blizzard = Spell("Blizzard", 10, 100, "black")
-meteor = Spell("Meteor", 20, 200, "black")
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 26, 680, "black")
+blizzard = Spell("Blizzard", 20, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black")
 quake = Spell("Quake", 14, 140, "black")
 
 # Create White Magic
-cure = Spell("Cure", 12, 120, "white")
-cura = Spell("Cura", 18, 200, "white")
+cure = Spell("Cure", 20, 500, "white")
+cura = Spell("Cura", 32, 1500, "white")
 
 # Create Some Items
 # Potions 
-potion = Item("Potion", "potion", "Heals 50 HP", 50)
-hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
-superpotion = Item("Super Potion", "potion", "Heals 500 HP", 500)
+potion = Item("Potion", "potion", "Heals 250 HP", 250)
+hipotion = Item("Hi-Potion", "potion", "Heals 1000 HP", 1000)
+superpotion = Item("Super Potion", "potion", "Heals 5000 HP", 5000)
 
 # Elixers
 elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
@@ -34,10 +34,10 @@ player_items = [{"item": potion, "quantity": 15}, {"item": hipotion, "quantity":
                 {"item": megaelixer, "quantity": 5}, {"item": grenade, "quantity": 10}]
 
 # Instantiate People
-player1 = Person("Valos:", 3460, 65, 60, 34, player_spells, player_items)
-player2 = Person("Range:", 2460, 65, 60, 34, player_spells, player_items)
-player3 = Person("Latho:", 8460, 65, 60, 34, player_spells, player_items)
-enemy = Person("Magus:", 1200, 65, 45, 25, [], [])
+player1 = Person("Valos:", 3460, 265, 180, 34, player_spells, player_items)
+player2 = Person("Range:", 2460, 220, 140, 34, player_spells, player_items)
+player3 = Person("Latho:", 8460, 290, 160, 34, player_spells, player_items)
+enemy = Person("Magus:", 11200, 120, 560, 25, [], [])
 
 players = [player1, player2, player3]
 
@@ -52,7 +52,7 @@ while running:
     print("==============================")
     
     print("\n\n")
-    print("NAME                    HP:                                  MP:")
+    print("NAME                    HP:                                    MP:")
     for player in players:
         player.get_stats()
         
